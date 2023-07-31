3f9cfa19e68a883b209769831dbaf2dd253bfc94
Sreehari <sreeharickotoor@gmail.com>
2023-08-01 00:14:54 +0530

    nav

--- recruit/src/App.js
+++ recruit/src/App.js
@@ -1,11 +1,14 @@
 import './index.css'
-import './App.css';
+import './App.css'
+import NavBar from './components/NavBar/nav.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <NavBar />
+      
+      
       </header>
     </div>
   );
